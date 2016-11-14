331f117f4e639cb935e3be3f6e4542b4dfe6d8b8
yunjiljjh <yunjiljjh@naver.com>
2016-11-14 14:35:27 +0900

    .
    .

--- MinDis/Main.java
+++ MinDis/Main.java
@@ -5,8 +5,15 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		InputReader reader=new InputReader("C:\\hw5\\131tsp.log");
-
 		
+		Node[] node = reader.node;
+		int numOfNode = reader.numOfNode;
+		
+		Dynamic dy = new Dynamic(numOfNode,node);
+		//Backtracking bt = new Backtracking(numOfNode,node);
+		//BranchAndBound bb= new BranchAndBound(numOfNode,node);
+		//Genetic gn = new Genetic(numOfNode,node);
+		//SimulatedAnnealing sa = new SimulatedAnnealing(numOfNode,node);
 	}
 
 }
