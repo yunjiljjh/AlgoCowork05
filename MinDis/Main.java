fe8ea1c6afb83a0e19902207efc9e588c83d6c6a
EuniceNam <caellime@gmail.com>
2016-11-24 23:16:56 +0900

    (to project file)

--- MinDis/Main.java
+++ MinDis/Main.java
@@ -1,19 +1,24 @@
+//hw5? hw56? CSI3108PGM_5tsp_2?
 package MinDis;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		InputReader reader=new InputReader("C:\\hw5\\131tsp.log");
+//		InputReader reader=new InputReader("C:\\hw5\\131tsp.log");
+		String path = "C:\\Users\\p34g\\Documents\\GitHub\\AlgoCowork05";
+		InputReader reader = new InputReader(path + "\\CSI3108PGM_5tsp_2\\13tsp.log");
 		
 		Node[] node = reader.node;
 		int numOfNode = reader.numOfNode;
 		
-		Dynamic dy = new Dynamic(numOfNode,node);
+//		Dynamic dy = new Dynamic(numOfNode,node);
 		//Backtracking bt = new Backtracking(numOfNode,node);
 		//BranchAndBound bb= new BranchAndBound(numOfNode,node);
 		//Genetic gn = new Genetic(numOfNode,node);
 		//SimulatedAnnealing sa = new SimulatedAnnealing(numOfNode,node);
+
+//		OutputWriter wr = new OutpurWriter(dy.result, "C:\\hw56\\id.txt");
+		OutputWriter wr = new OutputWriter("test", path + "\\Output\\test.txt");
 	}
 
 }
